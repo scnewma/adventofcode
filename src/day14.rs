28e14a444cedae25e161b1dee67d9d41c128dc8d
Shaun Newman <shaun.newman@hashicorp.com>
2022-12-14 10:03:40 -0700

    day 14: logic refactoring

--- src/day14.rs
+++ src/day14.rs
@@ -11,158 +11,96 @@ pub fn run(input: &str) -> anyhow::Result<SolveInfo> {
     })
 }
 
-pub fn part01(input: &str) -> anyhow::Result<u32> {
-    let mut map: HashMap<(usize, usize), Obstruction> = HashMap::new();
-    for Line(p1, p2) in parse_input(input)? {
-        if p1.0 == p2.0 {
-            // vertical
-            let (min, max) = (p1.1.min(p2.1), p1.1.max(p2.1));
-            for i in min..=max {
-                map.insert((p1.0, i), Obstruction::Rock);
-            }
-        } else {
-            // horizontal
-            let (min, max) = (p1.0.min(p2.0), p1.0.max(p2.0));
-            for i in min..=max {
-                map.insert((i, p1.1), Obstruction::Rock);
-            }
-        }
-    }
+const DELTAS: [(i32, i32); 3] = [(0, 1), (-1, 1), (1, 1)];
 
-    let max_y = *map
-        .iter()
-        .map(|((_x, y), _obs)| y)
-        .max()
-        .context("no max_y")?;
+pub fn part01(input: &str) -> anyhow::Result<u32> {
+    let mut map = parse_input(input)?;
+
+    let max_y = map.iter().map(|(p, _obs)| p.y).max().context("no max_y")?;
 
     let mut num_sand = 0;
-    let mut falling = (500usize, 0usize);
+    let mut sand = Point { x: 500, y: 0 };
     loop {
-        let (sand_x, sand_y) = falling;
-        // sand fell into void
-        if sand_y > max_y {
-            return Ok(num_sand);
-        }
-
-        // check down
-        match map.get(&(sand_x, sand_y + 1)) {
-            None => {
-                falling.1 += 1;
-                continue;
+        let before = sand;
+        for (dx, dy) in DELTAS {
+            let next = Point {
+                x: add(sand.x, dx),
+                y: add(sand.y, dy),
+            };
+            if map.get(&next).is_none() {
+                sand = next;
+                break;
             }
-            Some(_) => (),
         }
-        // check down-left
-        match map.get(&(sand_x - 1, sand_y + 1)) {
-            None => {
-                falling = (sand_x - 1, sand_y + 1);
-                continue;
-            }
-            Some(_) => (),
+        if sand == before {
+            map.insert(sand, Obstruction::Sand);
+            num_sand += 1;
+            sand = Point { x: 500, y: 0 };
         }
-        // check down-right
-        match map.get(&(sand_x + 1, sand_y + 1)) {
-            None => {
-                falling = (sand_x + 1, sand_y + 1);
-                continue;
-            }
-            Some(_) => (),
+        if sand.y > max_y {
+            break Ok(num_sand);
         }
-
-        // has settled at source
-        if (sand_x, sand_y) == (500, 0) {
-            return Ok(num_sand);
-        }
-        // has settled below source, reset and run again
-        map.insert((sand_x, sand_y), Obstruction::Sand);
-        num_sand += 1;
-        falling = (500, 0);
     }
 }
 
 pub fn part02(input: &str) -> anyhow::Result<u32> {
-    let mut map: HashMap<(usize, usize), Obstruction> = HashMap::new();
-    for Line(p1, p2) in parse_input(input)? {
-        if p1.0 == p2.0 {
-            // vertical
-            let (min, max) = (p1.1.min(p2.1), p1.1.max(p2.1));
-            for i in min..=max {
-                map.insert((p1.0, i), Obstruction::Rock);
-            }
-        } else {
-            // horizontal
-            let (min, max) = (p1.0.min(p2.0), p1.0.max(p2.0));
-            for i in min..=max {
-                map.insert((i, p1.1), Obstruction::Rock);
-            }
-        }
-    }
+    let map = parse_input(input)?;
 
-    let max_y = *map
-        .iter()
-        .map(|((_x, y), _obs)| y)
-        .max()
-        .context("no max_y")?;
+    let max_y = map.iter().map(|(p, _obs)| p.y).max().context("no max_y")?;
 
-    let floor = max_y + 2;
-
-    let mut map = ObstructionMap { grid: map, floor };
+    let mut map = ObstructionMap {
+        grid: map,
+        floor: max_y + 2,
+    };
 
     let mut num_sand = 0;
-    let mut falling = (500usize, 0usize);
+    let mut sand = Point { x: 500, y: 0 };
     loop {
-        let (sand_x, sand_y) = falling;
-        // check down
-        match map.get(&(sand_x, sand_y + 1)) {
-            None => {
-                falling.1 += 1;
-                continue;
+        let before = sand;
+        for (dx, dy) in DELTAS {
+            let next = Point {
+                x: add(sand.x, dx),
+                y: add(sand.y, dy),
+            };
+            if map.get(&next).is_none() {
+                sand = next;
+                break;
             }
-            Some(_) => (),
         }
-        // check down-left
-        match map.get(&(sand_x - 1, sand_y + 1)) {
-            None => {
-                falling = (sand_x - 1, sand_y + 1);
-                continue;
-            }
-            Some(_) => (),
+        if sand == (Point { x: 500, y: 0 }) {
+            break Ok(num_sand + 1);
         }
-        // check down-right
-        match map.get(&(sand_x + 1, sand_y + 1)) {
-            None => {
-                falling = (sand_x + 1, sand_y + 1);
-                continue;
-            }
-            Some(_) => (),
+        if sand == before {
+            map.insert(sand, Obstruction::Sand);
+            num_sand += 1;
+            sand = Point { x: 500, y: 0 };
         }
+    }
+}
 
-        num_sand += 1;
-        // has settled at source
-        if (sand_x, sand_y) == (500, 0) {
-            return Ok(num_sand);
-        }
-        // has settled below source, reset and run again
-        map.insert((sand_x, sand_y), Obstruction::Sand);
-        falling = (500, 0);
+fn add(u: usize, i: i32) -> usize {
+    if i.is_negative() {
+        u - i.unsigned_abs() as usize
+    } else {
+        u + i as usize
     }
 }
 
 struct ObstructionMap {
-    grid: HashMap<(usize, usize), Obstruction>,
+    grid: HashMap<Point, Obstruction>,
     floor: usize,
 }
 
 impl ObstructionMap {
-    fn get(&self, point: &(usize, usize)) -> Option<Obstruction> {
-        if point.1 == self.floor {
+    fn get(&self, point: &Point) -> Option<Obstruction> {
+        if point.y == self.floor {
             Some(Obstruction::Rock)
         } else {
             self.grid.get(point).cloned()
         }
     }
 
-    fn insert(&mut self, point: (usize, usize), obs: Obstruction) {
+    fn insert(&mut self, point: Point, obs: Obstruction) {
         self.grid.insert(point, obs);
     }
 }
@@ -176,23 +114,41 @@ enum Obstruction {
 #[derive(Debug, Clone, Copy)]
 struct Line(Point, Point);
 
-#[derive(Debug, Clone, Copy)]
-struct Point(usize, usize);
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct Point {
+    x: usize,
+    y: usize,
+}
 
-fn parse_input(input: &str) -> anyhow::Result<Vec<Line>> {
-    let mut lines = Vec::new();
+fn parse_input(input: &str) -> anyhow::Result<HashMap<Point, Obstruction>> {
+    let mut map: HashMap<Point, Obstruction> = HashMap::new();
     for line in input.lines() {
         let mut prev: Option<Point> = None;
         for point in line.split(" -> ") {
             let (left, right) = point.split_once(',').context("invalid point")?;
-            let point = Point(left.parse()?, right.parse()?);
+            let point = Point {
+                x: left.parse()?,
+                y: right.parse()?,
+            };
             if let Some(prev) = prev {
-                lines.push(Line(prev, point));
+                if prev.x == point.x {
+                    // vertical
+                    let (min, max) = (prev.y.min(point.y), prev.y.max(point.y));
+                    for i in min..=max {
+                        map.insert(Point { x: prev.x, y: i }, Obstruction::Rock);
+                    }
+                } else {
+                    // horizontal
+                    let (min, max) = (prev.x.min(point.x), prev.x.max(point.x));
+                    for i in min..=max {
+                        map.insert(Point { x: i, y: prev.y }, Obstruction::Rock);
+                    }
+                }
             }
             prev = Some(point);
         }
     }
-    Ok(lines)
+    Ok(map)
 }
 
 #[cfg(test)]
