1f3758c5ae868c58ac9e4fb31bd597e820dc2651
Shaun Newman <shaun.newman@hashicorp.com>
2022-12-18 10:19:35 -0700

    day 18: more cleanup

--- src/day18.rs
+++ src/day18.rs
@@ -1,6 +1,6 @@
 use itertools::iproduct;
 use partitions::PartitionVec;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 
 use crate::SolveInfo;
 
@@ -10,15 +10,6 @@ pub fn run(input: &str, _: bool) -> anyhow::Result<SolveInfo> {
         part02: part02(input)?.to_string(),
     })
 }
-
-const DELTAS: [(i32, i32, i32); 6] = [
-    (1i32, 0i32, 0i32),
-    (-1, 0, 0),
-    (0, 1, 0),
-    (0, -1, 0),
-    (0, 0, 1),
-    (0, 0, -1),
-];
 
 pub fn part01(input: &str) -> anyhow::Result<u32> {
     let cubes = parse_input(input);
@@ -36,14 +27,26 @@ fn part01_inner(cubes: &HashSet<(u32, u32, u32)>) -> u32 {
     surface_area
 }
 
-// largest x, y, or z was 19
+// To make code easier to write this one dimension of the 3D cube, it is 1 + max(dimension) of the
+// input
 const SIZE: u32 = 20;
 const VOL: u32 = SIZE * SIZE * SIZE;
 
+// Summary of algorithm:
+// Each position in the 3D cube is put in one of three categories of disjoint sets:
+//   * Escaped Air: Air block that is not "trapped"
+//   * Lava Droplet: All lava droplets (puzzle input)
+//   * Trapped Air: Basically everything that wasn't in one of the first two categories
+//
+// After we have categorized all of the positions in the 3D cube, we then calculate the surface
+// area of the trapped air. There are many different disjoint sets of trapped air (because they are
+// trapped in different locations). The total surface area of all pockets of trapped air is then
+// subtracted from the total surface area (i.e. part01).
 pub fn part02(input: &str) -> anyhow::Result<u32> {
     let cubes = parse_input(input);
     let surface_area = part01_inner(&cubes);
 
+    // partitions is a disjoint set of every position within the 3D cube
     let mut partitions: PartitionVec<(u32, u32, u32)> =
         PartitionVec::with_capacity(VOL as usize + 2);
     // create initial sets of self only
@@ -59,40 +62,41 @@ pub fn part02(input: &str) -> anyhow::Result<u32> {
     const LAVA_NODE: usize = (SIZE * SIZE * SIZE) as usize + 1;
     partitions.push((u32::max_value(), u32::max_value(), u32::max_value()));
 
-    for (x, y, z) in iproduct!(0..SIZE, 0..SIZE, 0..SIZE) {
-        // if this location is not a cube then it's an air block, union it with any
-        // neighboring air blocks
-        if !cubes.contains(&(x, y, z)) {
-            if x == 0 || y == 0 || z == 0 {
-                let me_index = index(&(x, y, z));
-                partitions.union(me_index, ESCAPE_NODE);
-            }
-            for delta in DELTAS {
-                if let Some(n) = get_neighbor(&(x, y, z), &delta) {
-                    if !cubes.contains(&n) {
-                        let idx1 = index(&(x, y, z));
-                        let idx2 = index(&n);
-                        if idx1 > VOL as usize || idx2 > VOL as usize {
-                            continue;
-                        }
-                        partitions.union(idx1, idx2);
-                    }
+    for pos in iproduct!(0..SIZE, 0..SIZE, 0..SIZE) {
+        if cubes.contains(&pos) {
+            // if this is a lava droplet, we put in in the set with the other lava droplets so that
+            // we can exclude these positions later
+            partitions.union(safe_index(&pos).unwrap(), LAVA_NODE);
+            continue;
+        }
+
+        // every air block on the outer edges of the cube are not trapped so we union them with
+        // the escape node to ensure they are not treated as trapped
+        if pos.0 == 0 || pos.1 == 0 || pos.2 == 0 {
+            partitions.union(safe_index(&pos).unwrap(), ESCAPE_NODE);
+        }
+
+        // this location is an air block, union it with any neighboring air blocks
+        for neighbor in neighbors(&pos) {
+            if !cubes.contains(&neighbor) {
+                if let (Some(pi), Some(ni)) = (safe_index(&pos), safe_index(&neighbor)) {
+                    partitions.union(pi, ni);
                 }
             }
-        } else {
-            let me_index = index(&(x, y, z));
-            partitions.union(me_index, LAVA_NODE);
         }
     }
 
     let mut trapped_surface_area = 0;
     let mut visited = HashSet::new();
     for (x, y, z) in iproduct!(0..SIZE, 0..SIZE, 0..SIZE) {
-        let idx = index(&(x, y, z));
+        // we are looking for trapped air, so we only need to check air that isn't in the same set
+        // as lava droplets or escaped air
+        let idx = safe_index(&(x, y, z)).unwrap();
         if partitions.same_set(idx, ESCAPE_NODE) || partitions.same_set(idx, LAVA_NODE) {
             continue;
         }
 
+        // expand the volume of trapped air to find the outer surface area of this pocket of air
         let mut queue = VecDeque::new();
         queue.push_back((x, y, z));
         while let Some(pos) = queue.pop_front() {
@@ -101,22 +105,15 @@ pub fn part02(input: &str) -> anyhow::Result<u32> {
             }
             visited.insert(pos);
 
-            for delta in DELTAS {
-                let n = get_neighbor(&pos, &delta);
-                if n.is_none() {
-                    continue;
-                }
-                let n = n.unwrap();
-
-                let ni = index(&n);
-                if ni > VOL as usize {
-                    continue;
-                }
-
-                if partitions.same_set(ni, LAVA_NODE) {
-                    trapped_surface_area += 1;
-                } else {
-                    queue.push_back(n);
+            for neighbor in neighbors(&pos) {
+                if let Some(ni) = safe_index(&neighbor) {
+                    if partitions.same_set(ni, LAVA_NODE) {
+                        // if this neighbor is a lava droplet then we found an edge to our air pocket
+                        trapped_surface_area += 1;
+                    } else {
+                        // this neighbor is an air block, we'll check that later
+                        queue.push_back(neighbor);
+                    }
                 }
             }
         }
@@ -125,8 +122,15 @@ pub fn part02(input: &str) -> anyhow::Result<u32> {
     Ok(surface_area - trapped_surface_area)
 }
 
-fn index((x, y, z): &(u32, u32, u32)) -> usize {
-    (z + y * SIZE + x * SIZE * SIZE) as usize
+// calculates an array index for a 3D point
+// returns None if the provided position is outside of the bounds of the cube
+fn safe_index((x, y, z): &(u32, u32, u32)) -> Option<usize> {
+    let idx = z + y * SIZE + x * SIZE * SIZE;
+    if idx > VOL {
+        None
+    } else {
+        Some(idx as usize)
+    }
 }
 
 fn neighbors(pos: &(u32, u32, u32)) -> Neighbors {
@@ -134,18 +138,6 @@ fn neighbors(pos: &(u32, u32, u32)) -> Neighbors {
         pos: *pos,
         deltas_index: 0,
         max_pos: SIZE,
-    }
-}
-
-fn get_neighbor(pos: &(u32, u32, u32), delta: &(i32, i32, i32)) -> Option<(u32, u32, u32)> {
-    if let (Some(x), Some(y), Some(z)) = (
-        pos.0.checked_add_signed(delta.0),
-        pos.1.checked_add_signed(delta.1),
-        pos.2.checked_add_signed(delta.2),
-    ) {
-        Some((x, y, z))
-    } else {
-        None
     }
 }
 
@@ -163,6 +155,15 @@ fn parse_input(input: &str) -> HashSet<(u32, u32, u32)> {
         .collect()
 }
 
+const DELTAS: [(i32, i32, i32); 6] = [
+    (1i32, 0i32, 0i32),
+    (-1, 0, 0),
+    (0, 1, 0),
+    (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+];
+
 struct Neighbors {
     pos: (u32, u32, u32),
     deltas_index: usize,
@@ -174,13 +175,16 @@ impl Iterator for Neighbors {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
+            // we don't have any more neighbors to calculate
             if self.deltas_index >= DELTAS.len() {
                 break None;
             }
             let delta = DELTAS[self.deltas_index];
             self.deltas_index += 1;
 
+            // we clamp the return positions of this iterator to be within 0..max_pos
             if let (Some(x), Some(y), Some(z)) = (
+                // checked_add_signed requires Rust 1.66, but yay for new features!
                 self.pos.0.checked_add_signed(delta.0),
                 self.pos.1.checked_add_signed(delta.1),
                 self.pos.2.checked_add_signed(delta.2),
