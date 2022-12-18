0e986fddd8a398e0a059d764eb79f333f64f7667
Shaun Newman <shaun.newman@hashicorp.com>
2022-12-18 10:23:01 -0700

    day 18: benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -19,7 +19,7 @@ macro_rules! gen_benches {
 
 gen_benches![
     day01, day02, day03, day04, day05, day06, day07, day08, day09, day12, day13, day14, day15,
-    day16
+    day16, day18
 ];
 
 criterion_group!(benches, criterion_benchmark);
