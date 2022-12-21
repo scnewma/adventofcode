66424d66e946c2f2c0580b9ab684ee0a3c2bf20b
Shaun Newman <shaun.newman@hashicorp.com>
2022-12-20 17:43:11 -0700

    day 20: part 2 + some refactoring

--- src/day20.rs
+++ src/day20.rs
@@ -16,29 +16,50 @@ pub fn part01(input: &str) -> anyhow::Result<isize> {
         process_results(input.lines().map(|line| line.parse::<isize>()), |it| {
             it.collect()
         })?;
-    let mut mixed: Vec<_> = file.clone().into_iter().enumerate().collect();
-    for (i, num) in file.iter().enumerate() {
-        let current_pos = mixed.iter().position(|&item| item == (i, *num)).unwrap();
-        // verify that there wasn't some sort of bug in previous iterations of the loop
-        assert_eq!(mixed.remove(current_pos), (i, *num));
-        let next_pos = next_pos(current_pos, *num, &mixed);
-        mixed.insert(next_pos, (i, *num));
-    }
-
-    let idx_zero = mixed.iter().position(|&item| item.1 == 0).unwrap();
-    Ok([1000, 2000, 3000]
-        .iter()
-        .map(|idx| {
-            let n_after_zero = idx % mixed.len();
-            let final_idx = (idx_zero + n_after_zero) % mixed.len();
-            let n = mixed[final_idx].1;
-            n
-        })
-        .sum())
+    Ok(grove_coordinates(file, 1))
 }
 
-pub fn part02(input: &str) -> anyhow::Result<u32> {
-    Ok(0)
+const DECRYPTION_KEY: isize = 811589153;
+
+pub fn part02(input: &str) -> anyhow::Result<isize> {
+    let file: Vec<isize> =
+        process_results(input.lines().map(|line| line.parse::<isize>()), |it| {
+            it.map(|n| n * DECRYPTION_KEY).collect()
+        })?;
+    Ok(grove_coordinates(file, 10))
+}
+
+fn grove_coordinates(file: Vec<isize>, rounds: usize) -> isize {
+    let mut mixed: Vec<_> = file.clone().into_iter().enumerate().collect();
+    for _ in 0..rounds {
+        for (i, num) in file.iter().enumerate() {
+            // find where this item currently is in the array, accounting for both i and num
+            // because the nums could repeat
+            let current_pos = mixed.iter().position(|&item| item == (i, *num)).unwrap();
+
+            // from here we move this number to it's new home by removing it from the array and
+            // re-inserting it at the new location. all new position calculations are performed on
+            // the smaller array
+
+            // verify that there wasn't some sort of bug in previous iterations of the loop
+            assert_eq!(mixed.remove(current_pos), (i, *num));
+            let next_pos = next_pos(current_pos, *num, &mixed);
+            mixed.insert(next_pos, (i, *num));
+        }
+    }
+
+    // sum up the 1000th, 2000th and 3000th position after 0
+    let idx_zero = mixed.iter().position(|&item| item.1 == 0).unwrap();
+    [1000, 2000, 3000]
+        .iter()
+        .map(|idx| {
+            // how far into the array the coordinate would be
+            let coord_offset = idx % mixed.len();
+            // the final coordinate, adjusted to be coord_offset past zero
+            let idx_num = (idx_zero + coord_offset) % mixed.len();
+            mixed[idx_num].1
+        })
+        .sum()
 }
 
 fn next_pos<V>(pos: usize, amt: isize, vec: &Vec<V>) -> usize {
@@ -82,15 +103,15 @@ mod tests {
         assert_eq!(7584, ans);
     }
 
-    // #[test]
-    // fn test_part_two_sample() {
-    //     let ans = part02(SAMPLE).unwrap();
-    //     assert_eq!(0, ans);
-    // }
+    #[test]
+    fn test_part_two_sample() {
+        let ans = part02(SAMPLE).unwrap();
+        assert_eq!(1623178306, ans);
+    }
 
-    // #[test]
-    // fn test_part_two() {
-    //     let ans = part02(INPUT).unwrap();
-    //     assert_eq!(0, ans);
-    // }
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(4907679608191, ans);
+    }
 }
