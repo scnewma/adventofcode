8734183a5d4fe1ddf8f0a6fab819555360397bb1
Shaun Newman <shaun.newman@hashicorp.com>
2022-12-20 17:28:42 -0700

    day 20: part 1

--- src/day20.rs
+++ src/day20.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::cmp::Ordering;
 
 use itertools::process_results;
 
@@ -11,62 +11,28 @@ pub fn run(input: &str, _: bool) -> anyhow::Result<SolveInfo> {
     })
 }
 
-pub fn part01(input: &str) -> anyhow::Result<i32> {
-    let file: Vec<i32> = process_results(input.lines().map(|line| line.parse::<i32>()), |it| {
-        it.collect()
-    })?;
-    let mut zero_pair = (0, 0);
-    let mut positions = HashMap::new();
-    for (i, num) in file.iter().enumerate() {
-        positions.insert((i, *num), i);
-        if *num == 0 {
-            zero_pair = (i, *num);
-        }
-    }
+pub fn part01(input: &str) -> anyhow::Result<isize> {
+    let file: Vec<isize> =
+        process_results(input.lines().map(|line| line.parse::<isize>()), |it| {
+            it.collect()
+        })?;
     let mut mixed: Vec<_> = file.clone().into_iter().enumerate().collect();
     for (i, num) in file.iter().enumerate() {
-        // println!(
-        //     "current={:?} i={i} num={num}",
-        //     mixed.iter().map(|(_, n)| n).collect::<Vec<_>>()
-        // );
-        let current_pos = *positions.get(&(i, *num)).unwrap();
-        let max_rotation = num.abs() as usize % file.len();
-        // println!("  current_pos={current_pos} max_rotation={max_rotation}");
-        match num.cmp(&0) {
-            Ordering::Equal => (),
-            Ordering::Less => {
-                for j in 0..max_rotation {
-                    let j_idx = index_rotate_left(current_pos, j, file.len());
-                    let next_idx = index_rotate_left(current_pos, j + 1, file.len());
-                    // println!("  mixed[{}] = mixed[{}]", j_idx, next_idx);
-                    mixed[j_idx] = mixed[next_idx];
-                    positions.insert(mixed[j_idx], j_idx);
-                }
-                let new_pos = index_rotate_left(current_pos, max_rotation, file.len());
-                mixed[new_pos] = (i, *num);
-                positions.insert((i, *num), new_pos);
-            }
-            Ordering::Greater => {
-                for j in 0..max_rotation {
-                    let j_idx = (current_pos + j) % file.len();
-                    let next_idx = (current_pos + j + 1) % file.len();
-                    mixed[j_idx] = mixed[next_idx];
-                    positions.insert(mixed[j_idx], j_idx);
-                }
-                let new_pos = (current_pos + max_rotation) % file.len();
-                mixed[new_pos] = (i, *num);
-                positions.insert((i, *num), new_pos);
-            }
-        }
+        let current_pos = mixed.iter().position(|&item| item == (i, *num)).unwrap();
+        // verify that there wasn't some sort of bug in previous iterations of the loop
+        assert_eq!(mixed.remove(current_pos), (i, *num));
+        let next_pos = next_pos(current_pos, *num, &mixed);
+        mixed.insert(next_pos, (i, *num));
     }
 
-    let idx_zero = *positions.get(&zero_pair).unwrap();
+    let idx_zero = mixed.iter().position(|&item| item.1 == 0).unwrap();
     Ok([1000, 2000, 3000]
         .iter()
         .map(|idx| {
             let n_after_zero = idx % mixed.len();
             let final_idx = (idx_zero + n_after_zero) % mixed.len();
-            mixed[final_idx].1
+            let n = mixed[final_idx].1;
+            n
         })
         .sum())
 }
@@ -75,12 +41,26 @@ pub fn part02(input: &str) -> anyhow::Result<u32> {
     Ok(0)
 }
 
-fn index_rotate_left(pos: usize, amt: usize, max: usize) -> usize {
-    if amt > pos {
-        max - (amt - pos)
-    } else {
-        pos - amt
-    }
+fn next_pos<V>(pos: usize, amt: isize, vec: &Vec<V>) -> usize {
+    let num_rotations = amt.abs() as usize % vec.len();
+    let next_pos = match amt.cmp(&0) {
+        Ordering::Equal => pos,
+        Ordering::Less => {
+            if num_rotations > pos {
+                vec.len() - num_rotations + pos
+            } else {
+                pos - num_rotations
+            }
+        }
+        Ordering::Greater => {
+            if pos + num_rotations > vec.len() {
+                (pos + num_rotations) % vec.len()
+            } else {
+                pos + num_rotations
+            }
+        }
+    };
+    next_pos
 }
 
 #[cfg(test)]
@@ -96,11 +76,11 @@ mod tests {
         assert_eq!(3, ans);
     }
 
-    // #[test]
-    // fn test_part_one() {
-    //     let ans = part01(INPUT).unwrap();
-    //     assert_eq!(1766, ans);
-    // }
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(7584, ans);
+    }
 
     // #[test]
     // fn test_part_two_sample() {
