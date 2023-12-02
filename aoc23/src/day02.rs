3ae42fd4473dfdae157b18b8e5bd2146e969fe6d
Shaun Newman <newman350z@gmail.com>
2023-12-01 22:36:29 -0700

    2023: day 2 initial solution

--- aoc23/src/day02.rs
+++ aoc23/src/day02.rs
@@ -1,3 +1,6 @@
+use anyhow::Context;
+use std::str::FromStr;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +8,101 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<u32> {
+    const MAX_RED: u32 = 12;
+    const MAX_GREEN: u32 = 13;
+    const MAX_BLUE: u32 = 14;
+
+    let sum = input
+        .lines()
+        .flat_map(Game::from_str)
+        .filter(|game| {
+            game.hands
+                .iter()
+                .all(|hand| hand.0 <= MAX_RED && hand.1 <= MAX_GREEN && hand.2 <= MAX_BLUE)
+        })
+        .map(|game| game.id)
+        .sum();
+    Ok(sum)
 }
 
-pub fn part02(input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<u32> {
+    let sum = input
+        .lines()
+        .flat_map(Game::from_str)
+        .map(|game| {
+            game.hands
+                .into_iter()
+                .reduce(|acc, e| RGB(acc.0.max(e.0), acc.1.max(e.1), acc.2.max(e.2)))
+                .unwrap()
+        })
+        .map(|hand| hand.0 * hand.1 * hand.2)
+        .sum();
+    Ok(sum)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    hands: Vec<RGB>,
+}
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day02.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day02.input.txt");
+impl FromStr for Game {
+    type Err = anyhow::Error;
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (game_no, game_info) = s
+            .split_once(": ")
+            .context("expected format Game #: <hands>")?;
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+        let game_no = game_no
+            .strip_prefix("Game ")
+            .context("missing \"Game \" prefix")?;
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
+        let id: u32 = game_no.parse().context("parsing game number")?;
 
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+        let mut hands = Vec::new();
+        for hand_info in game_info.split("; ") {
+            let mut rgb = RGB::default();
+            for cube_info in hand_info.split(", ") {
+                let (count, color) = cube_info
+                    .split_once(" ")
+                    .context("expected cube count to be in format \"<count> <color>\"")?;
+
+                let count: u32 = count.parse().context("parsing cube count")?;
+
+                match color {
+                    "red" => rgb.0 = count,
+                    "green" => rgb.1 = count,
+                    "blue" => rgb.2 = count,
+                    _ => unreachable!("unexpected color {color}"),
+                }
+            }
+            hands.push(rgb);
+        }
+
+        Ok(Game { id, hands })
+    }
+}
+
+#[derive(Debug, Default)]
+struct RGB(u32, u32, u32);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = include_str!("../inputs/day02.input.txt");
+
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(2720, ans);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(71535, ans);
+    }
+}
