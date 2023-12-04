0cc0e7eb794b0ffd34cf474eaa2d5353b6f4215f
Shaun Newman <newman350z@gmail.com>
2023-12-03 22:38:17 -0700

    2023: day 4 solution

--- aoc23/src/day04.rs
+++ aoc23/src/day04.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +7,61 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    Ok(parse_input(input)
+        .map(|(winning, mine)| {
+            let count = winning.intersection(&mine).count();
+            if count == 0 {
+                0
+            } else {
+                2usize.pow(count as u32 - 1)
+            }
+        })
+        .sum())
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    let mut card_copies = vec![1; input.lines().count()];
+    for (i, (winning, mine)) in parse_input(input).enumerate() {
+        for j in 1..=winning.intersection(&mine).count() {
+            card_copies[i + j] += card_copies[i];
+        }
+    }
+    Ok(card_copies.iter().sum())
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn parse_input(input: &str) -> impl Iterator<Item = (HashSet<u32>, HashSet<u32>)> + '_ {
+    input.lines().map(|line| {
+        let (_, info) = line.split_once(": ").unwrap();
+        let (winning, mine) = info.split_once(" | ").unwrap();
+        let winning: HashSet<u32> = winning
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        let mine: HashSet<u32> = mine
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day04.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day04.input.txt");
+        (winning, mine)
+    })
+}
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+    const INPUT: &'static str = include_str!("../inputs/day04.input.txt");
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(20117, ans);
+    }
 
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(13768818, ans);
+    }
+}
