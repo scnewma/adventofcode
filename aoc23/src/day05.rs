d0b70a52915efcf606a0afb3fc54a30046f05152
Shaun Newman <newman350z@gmail.com>
2023-12-05 09:51:02 -0700

    2023: day 5 cleanup to be more idiomatic

--- aoc23/src/day05.rs
+++ aoc23/src/day05.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -6,20 +8,13 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
 }
 
 pub fn part01(input: &str) -> anyhow::Result<u64> {
-    let mut sections = input.split("\n\n");
-    let (_, seeds) = sections.next().unwrap().split_once(": ").unwrap();
-    let seeds: Vec<u64> = seeds
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
+    let Input { seeds, mappings } = parse_input(input);
 
-    let mappings: Vec<_> = sections.map(parse_map).collect();
     let mut closest = u64::max_value();
     for seed in seeds {
         let mut lookup = seed;
         for map in mappings.iter() {
-            for m in map {
-                let (dst, src, amt) = m;
+            for (dst, src, amt) in map {
                 if (*src..*src + amt).contains(&lookup) {
                     lookup = dst + (lookup - src);
                     break;
@@ -32,26 +27,19 @@ pub fn part01(input: &str) -> anyhow::Result<u64> {
 }
 
 pub fn part02(input: &str) -> anyhow::Result<u64> {
-    let mut sections = input.split("\n\n");
-    let (_, seeds) = sections.next().unwrap().split_once(": ").unwrap();
+    let Input {
+        seeds: nums,
+        mappings,
+    } = parse_input(input);
 
-    let nums: Vec<_> = seeds
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    let mappings: Vec<_> = sections.map(parse_map).collect();
     let mut closest = u64::max_value();
-    for i in (0..nums.len()).step_by(2) {
-        let (src, amt) = (nums[i], nums[i + 1]);
-
+    for (src, amt) in nums.into_iter().tuples() {
         let mut j = 0u64;
         while j < amt {
             let mut skip = u64::max_value();
             let mut lookup = src + j;
             for map in mappings.iter() {
-                for m in map {
-                    let (dst, src, amt) = m;
+                for (dst, src, amt) in map {
                     if *src <= lookup && lookup < *src + amt {
                         // this is the main mechanism to reduce work. calculate the the upper bound
                         // for inputs that would produce a linear result. you can safely skip the
@@ -73,6 +61,29 @@ pub fn part02(input: &str) -> anyhow::Result<u64> {
         }
     }
     Ok(closest)
+}
+
+struct Input {
+    // for part 2 this is alternating "start seed + range"
+    seeds: Vec<u64>,
+    mappings: Vec<Vec<(u64, u64, u64)>>,
+}
+
+fn parse_input(s: &str) -> Input {
+    let mut sections = s.split("\n\n");
+    let (_, seeds) = sections.next().unwrap().split_once(": ").unwrap();
+
+    let nums: Vec<_> = seeds
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    // ordered stack of "maps" (i.e. seed-to-soil, soil-to-fertilizer, etc.)
+    let mappings: Vec<_> = sections.map(parse_map).collect();
+    Input {
+        seeds: nums,
+        mappings,
+    }
 }
 
 fn parse_map(s: &str) -> Vec<(u64, u64, u64)> {
