b6b81d3cb80d847fe4acfdfb59628fdffb32d3ae
Shaun Newman <newman350z@gmail.com>
2023-12-04 22:50:29 -0700

    2023: day 5 brute-force solution
    part 2 takes about 3 minutes to solve

--- aoc23/src/day05.rs
+++ aoc23/src/day05.rs
@@ -5,12 +5,77 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<u64> {
+    let mut sections = input.split("\n\n");
+    let (_, seeds) = sections.next().unwrap().split_once(": ").unwrap();
+    let seeds: Vec<u64> = seeds
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mappings: Vec<_> = sections.map(parse_map).collect();
+    let mut closest = u64::max_value();
+    for seed in seeds {
+        let mut lookup = seed;
+        for map in mappings.iter() {
+            for m in map {
+                let (dst, src, amt) = m;
+                if (*src..*src + amt).contains(&lookup) {
+                    lookup = dst + (lookup - src);
+                    break;
+                }
+            }
+        }
+        closest = closest.min(lookup);
+    }
+    Ok(closest)
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<u64> {
+    let mut sections = input.split("\n\n");
+    let (_, seeds) = sections.next().unwrap().split_once(": ").unwrap();
+
+    let nums: Vec<_> = seeds
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let mut seeds = vec![];
+    for i in (0..nums.len()).step_by(2) {
+        let (src, amt) = (nums[i], nums[i + 1]);
+        for i in 0..amt {
+            seeds.push(src + i);
+        }
+    }
+
+    let mappings: Vec<_> = sections.map(parse_map).collect();
+    let mut closest = u64::max_value();
+    for seed in seeds {
+        let mut lookup = seed;
+        for map in mappings.iter() {
+            for m in map {
+                let (dst, src, amt) = m;
+                if (*src..*src + amt).contains(&lookup) {
+                    lookup = dst + (lookup - src);
+                    break;
+                }
+            }
+        }
+        closest = closest.min(lookup);
+    }
+    Ok(closest)
+}
+
+fn parse_map(s: &str) -> Vec<(u64, u64, u64)> {
+    s.lines()
+        .skip(1)
+        .map(|line| {
+            let r: Vec<u64> = line
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+            (r[0], r[1], r[2])
+        })
+        .collect()
 }
 
 // #[cfg(test)]
