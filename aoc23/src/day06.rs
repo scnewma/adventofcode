b858cc657f45aa17fc3e4ce7ac66bf5f664033ee
Shaun Newman <newman350z@gmail.com>
2023-12-05 22:25:59 -0700

    2023: day 6 solution

--- aoc23/src/day06.rs
+++ aoc23/src/day06.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +7,74 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<u64> {
+    let mut lines = input.lines();
+
+    let mut get_numbers = || -> Vec<u64> {
+        let (_, rest) = lines.next().unwrap().split_once(':').unwrap();
+        let ns: Vec<u64> = rest
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        ns
+    };
+
+    let times = get_numbers();
+    let distances = get_numbers();
+
+    let ans = (0..times.len())
+        .map(|i| winning_strategies(times[i], distances[i]))
+        .product();
+    Ok(ans)
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<u64> {
+    let mut lines = input.lines();
+
+    let mut get_number = || -> anyhow::Result<u64> {
+        let n: u64 = lines
+            .next()
+            .context("no line")?
+            .split_once(':')
+            .map(|(_, rest)| rest)
+            .context("wrong format")?
+            .replace(' ', "")
+            .parse()?;
+        Ok(n)
+    };
+    let time = get_number()?;
+    let distance = get_number()?;
+
+    Ok(winning_strategies(time, distance))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn winning_strategies(time: u64, min_distance: u64) -> u64 {
+    let mut winning_strategies = 0;
+    for t in 1..time {
+        let left = time - t;
+        let traveled = left * t;
+        if traveled > min_distance {
+            winning_strategies += 1;
+        }
+    }
+    winning_strategies
+}
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day06.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day06.input.txt");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+    const INPUT: &'static str = include_str!("../inputs/day06.input.txt");
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(2344708, ans);
+    }
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
-
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(30125202, ans);
+    }
+}
