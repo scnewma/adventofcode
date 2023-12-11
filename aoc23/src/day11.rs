4d9e8112a082c5544f922671cdcab2054accd555
Shaun Newman <newman350z@gmail.com>
2023-12-10 22:48:37 -0700

    2023: day 11 solution

--- aoc23/src/day11.rs
+++ aoc23/src/day11.rs
@@ -1,3 +1,5 @@
+use itertools::{iproduct, Itertools};
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +7,77 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<u64> {
+    Ok(solve(input, 1))
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<u64> {
+    Ok(solve(input, 999_999))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn solve(input: &str, expansion: usize) -> u64 {
+    let cosmos = input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day11.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day11.input.txt");
+    let mut expanded_rows = vec![];
+    let mut expanded_cols = vec![];
+    for (r, row) in cosmos.iter().enumerate() {
+        if row.iter().all(|ch| *ch == '.') {
+            expanded_rows.push(r);
+        }
+    }
+    for col in 0..cosmos[0].len() {
+        let mut all_empty = true;
+        for row in &cosmos {
+            all_empty = all_empty && row[col] == '.'
+        }
+        if all_empty {
+            expanded_cols.push(col);
+        }
+    }
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+    let galaxies = iproduct!(0..cosmos.len(), 0..cosmos[0].len())
+        .filter(|(row, col)| cosmos[*row][*col] == '#')
+        .collect_vec();
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+    let mut sum = 0u64;
+    for (a, b) in galaxies.iter().tuple_combinations() {
+        let mut growth = 0;
+        for row in (a.0.min(b.0))..(a.0.max(b.0)) {
+            if expanded_rows.contains(&row) {
+                growth += expansion;
+            }
+        }
+        for col in (a.1.min(b.1))..(a.1.max(b.1)) {
+            if expanded_cols.contains(&col) {
+                growth += expansion;
+            }
+        }
+        // manhattan distance
+        let distance = a.0.abs_diff(b.0) + a.1.abs_diff(b.1) + growth;
+        sum += distance as u64;
+    }
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
+    sum
+}
 
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = include_str!("../inputs/day11.input.txt");
+
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(9734203, ans);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(568914596391, ans);
+    }
+}
