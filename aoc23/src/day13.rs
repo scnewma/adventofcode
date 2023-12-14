6709079d0b2795bddc3ec6032089da03ebe04d85
Shaun Newman <newman350z@gmail.com>
2023-12-13 17:14:49 -0700

    2023: day 13 cleanup

--- aoc23/src/day13.rs
+++ aoc23/src/day13.rs
@@ -8,40 +8,49 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
 }
 
 pub fn part01(input: &str) -> anyhow::Result<usize> {
-    let patterns = input
-        .split("\n\n")
+    let patterns = parse_input(input);
+
+    Ok(patterns
+        .into_iter()
         .map(|pattern| {
-            pattern
-                .lines()
-                .map(|line| line.chars().collect_vec())
-                .collect_vec()
+            let reflections = lines_of_reflection(&pattern);
+            assert!(reflections.len() == 1);
+            reflections[0].value()
         })
-        .collect_vec();
-    let mut total = 0;
-    for pattern in patterns {
-        for row in &pattern {
-            for col in row {
-                print!("{}", col);
-            }
-            println!();
-        }
-        let reflections = lines_of_reflection(&pattern, "PART 1");
-        assert!(reflections.len() == 1);
-        match reflections[0] {
-            LineOfReflection::Horizontal(n) => {
-                total += n;
-            }
-            LineOfReflection::Vertical(n) => {
-                total += n * 100;
-            }
-        }
-    }
-    Ok(total)
+        .sum())
 }
 
 pub fn part02(input: &str) -> anyhow::Result<usize> {
-    println!("part02");
-    let patterns = input
+    let patterns = parse_input(input);
+
+    Ok(patterns
+        .into_iter()
+        .map(|mut pattern| {
+            let orig_reflections = lines_of_reflection(&pattern);
+            assert!(orig_reflections.len() == 1);
+            let orig_reflection = orig_reflections[0];
+
+            for (r, c) in iproduct!(0..pattern.len(), 0..pattern[0].len()) {
+                pattern[r][c] = if pattern[r][c] == '#' { '.' } else { '#' };
+
+                let reflections = lines_of_reflection(&pattern);
+                for reflection in reflections {
+                    if reflection != orig_reflection {
+                        return reflection.value();
+                    }
+                }
+
+                // reset
+                pattern[r][c] = if pattern[r][c] == '#' { '.' } else { '#' };
+            }
+
+            unreachable!("no alternative reflection found");
+        })
+        .sum())
+}
+
+fn parse_input(input: &str) -> Vec<Vec<Vec<char>>> {
+    input
         .split("\n\n")
         .map(|pattern| {
             pattern
@@ -49,128 +58,7 @@ pub fn part02(input: &str) -> anyhow::Result<usize> {
                 .map(|line| line.chars().collect_vec())
                 .collect_vec()
         })
-        .collect_vec();
-
-    let mut total = 0;
-    for pattern in patterns {
-        let orig_reflections = lines_of_reflection(&pattern, "ORIGINAL");
-        assert!(orig_reflections.len() == 1);
-        let orig_reflection = orig_reflections[0];
-
-        let mut found = false;
-        'permutations: for (r, c) in iproduct!(0..pattern.len(), 0..pattern[0].len()) {
-            let mut perm = pattern.clone();
-            perm[r][c] = if perm[r][c] == '#' { '.' } else { '#' };
-
-            let reflections = lines_of_reflection(&perm, "SMUDGED");
-            for reflection in reflections {
-                if reflection != orig_reflection {
-                    total += match reflection {
-                        LineOfReflection::Horizontal(n) => n,
-                        LineOfReflection::Vertical(n) => 100 * n,
-                    };
-                    found = true;
-                    break 'permutations;
-                }
-            }
-        }
-
-        assert!(found);
-
-        // TODO: there is a bug in the below, but i'm not sure what it is. it should be much more
-        // efficient though...
-        //
-        // let orig_reflections = lines_of_reflection(&pattern, "ORIGINAL");
-        // assert!(orig_reflections.len() == 1);
-        // let orig_reflection = orig_reflections[0];
-
-        // let before = total;
-        // for i in 0..pattern.len() {
-        //     for j in i + 1..pattern.len() {
-        //         let a = String::from_iter(&pattern[i]);
-        //         let b = String::from_iter(&pattern[j]);
-        //         if edit_distance(&a, &b) != 1 {
-        //             continue;
-        //         }
-
-        //         let mut updated = pattern.clone();
-        //         updated[i] = pattern[j].clone();
-        //         let reflections = lines_of_reflection(&updated, "ROW MUTATION 1");
-        //         for reflection in reflections {
-        //             if reflection != orig_reflection {
-        //                 total += match reflection {
-        //                     LineOfReflection::Horizontal(n) => n,
-        //                     LineOfReflection::Vertical(n) => 100 * n,
-        //                 };
-        //                 continue 'patterns;
-        //             }
-        //         }
-
-        //         let mut updated = pattern.clone();
-        //         updated[j] = pattern[i].clone();
-        //         let reflections = lines_of_reflection(&updated, "ROW MUTATION 2");
-        //         for reflection in reflections {
-        //             if reflection != orig_reflection {
-        //                 total += match reflection {
-        //                     LineOfReflection::Horizontal(n) => n,
-        //                     LineOfReflection::Vertical(n) => 100 * n,
-        //                 };
-        //                 continue 'patterns;
-        //             }
-        //         }
-        //     }
-        // }
-
-        // let pattern = transpose(&pattern);
-        // for i in 0..pattern.len() {
-        //     for j in i + 1..pattern.len() {
-        //         let a = String::from_iter(&pattern[i]);
-        //         let b = String::from_iter(&pattern[j]);
-        //         if edit_distance(&a, &b) != 1 {
-        //             continue;
-        //         }
-
-        //         let mut updated = pattern.clone();
-        //         updated[i] = pattern[j].clone();
-        //         let reflections = lines_of_reflection(&updated, "COL MUTATION 1");
-        //         for reflection in reflections {
-        //             if reflection != orig_reflection {
-        //                 total += match reflection {
-        //                     LineOfReflection::Horizontal(n) => n,
-        //                     LineOfReflection::Vertical(n) => 100 * n,
-        //                 };
-        //                 continue 'patterns;
-        //             }
-        //         }
-
-        //         let mut updated = pattern.clone();
-        //         updated[j] = pattern[i].clone();
-        //         let reflections = lines_of_reflection(&updated, "COL MUTATION 2");
-        //         for reflection in reflections {
-        //             if reflection != orig_reflection {
-        //                 total += match reflection {
-        //                     LineOfReflection::Horizontal(n) => n,
-        //                     LineOfReflection::Vertical(n) => 100 * n,
-        //                 };
-        //                 continue 'patterns;
-        //             }
-        //         }
-        //     }
-        // }
-        // if before == total {
-        //     println!("no reflection");
-        // }
-    }
-    Ok(total)
-}
-
-fn print_pattern(pattern: &Vec<Vec<char>>) {
-    for row in pattern {
-        for col in row {
-            print!("{}", col);
-        }
-        println!();
-    }
+        .collect_vec()
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -179,21 +67,26 @@ enum LineOfReflection {
     Vertical(usize),
 }
 
-fn lines_of_reflection(pattern: &Vec<Vec<char>>, label: &str) -> Vec<LineOfReflection> {
-    println!("--- {label}");
-    print_pattern(pattern);
+impl LineOfReflection {
+    fn value(&self) -> usize {
+        match self {
+            LineOfReflection::Horizontal(n) => *n,
+            LineOfReflection::Vertical(n) => *n * 100,
+        }
+    }
+}
+
+fn lines_of_reflection(pattern: &Vec<Vec<char>>) -> Vec<LineOfReflection> {
     let mut reflections = Vec::new();
     reflections.append(
         &mut reflects_horizontal(pattern)
             .into_iter()
-            .inspect(|n| println!("reflects horizontal at {}", n))
             .map(LineOfReflection::Horizontal)
             .collect_vec(),
     );
     reflections.append(
         &mut reflects_vertical(pattern)
             .into_iter()
-            .inspect(|n| println!("reflects vertical at {}", n))
             .map(LineOfReflection::Vertical)
             .collect_vec(),
     );
