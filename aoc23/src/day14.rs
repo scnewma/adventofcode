c6a6558b37f914b3993e234f63c0bd27fc2f724e
Shaun Newman <newman350z@gmail.com>
2023-12-13 23:46:59 -0700

    2023: day 14 solution

--- aoc23/src/day14.rs
+++ aoc23/src/day14.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +9,143 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    let mut grid = parse_input(input);
+    tilt_north(&mut grid);
+    Ok(calculate_load(&grid))
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    let mut grid = parse_input(input);
+
+    let mut cache: HashMap<Vec<Vec<char>>, usize> = HashMap::new();
+
+    let mut i = 0;
+    let cycle_len = loop {
+        spin_cycle(&mut grid);
+
+        // detect cycle
+        if let Some(j) = cache.get(&grid) {
+            break i - j;
+        }
+
+        cache.insert(grid.clone(), i);
+        i += 1;
+    };
+
+    // i+1 to account for the last spin cycle
+    let rem = (1000000000 - (i + 1)) % cycle_len;
+    for _ in 0..rem {
+        spin_cycle(&mut grid);
+    }
+    Ok(calculate_load(&grid))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn parse_input(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec()
+}
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day14.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day14.input.txt");
+fn calculate_load(grid: &Vec<Vec<char>>) -> usize {
+    let mut total = 0;
+    for col in 0..grid[0].len() {
+        for row in 0..grid.len() {
+            if grid[row][col] == 'O' {
+                total += grid.len() - row;
+            }
+        }
+    }
+    total
+}
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+fn spin_cycle(grid: &mut Vec<Vec<char>>) {
+    tilt_north(grid);
+    tilt_west(grid);
+    tilt_south(grid);
+    tilt_east(grid);
+}
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+fn tilt_north(grid: &mut Vec<Vec<char>>) {
+    for col in 0..grid[0].len() {
+        for row in 0..grid.len() {
+            if grid[row][col] == '.' {
+                for i in row + 1..grid.len() {
+                    match grid[i][col] {
+                        'O' => {
+                            grid[row][col] = 'O';
+                            grid[i][col] = '.';
+                            break;
+                        }
+                        '#' => break,
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+}
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
+fn tilt_south(grid: &mut Vec<Vec<char>>) {
+    let mut tilted = rotate(&rotate(grid));
+    tilt_north(&mut tilted);
+    *grid = rotate(&rotate(&tilted));
+}
 
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+fn tilt_east(grid: &mut Vec<Vec<char>>) {
+    let mut rotated = rotate_left(grid.clone());
+    tilt_north(&mut rotated);
+    *grid = rotate(&rotated);
+}
+
+fn tilt_west(grid: &mut Vec<Vec<char>>) {
+    let mut rotated = rotate(grid);
+    tilt_north(&mut rotated);
+    *grid = rotate_left(rotated);
+}
+
+fn transpose<T: Clone>(matrix: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut t = vec![Vec::with_capacity(matrix.len()); matrix.len()];
+    for r in matrix {
+        for i in 0..r.len() {
+            t[i].push(r[i].clone());
+        }
+    }
+    t
+}
+
+fn rotate<T: Clone>(matrix: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut r = transpose(matrix);
+    for col in &mut r {
+        col.reverse();
+    }
+    r
+}
+
+fn rotate_left<T: Clone>(matrix: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut r = matrix;
+    for col in &mut r {
+        col.reverse();
+    }
+    transpose(&r)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = include_str!("../inputs/day14.input.txt");
+
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(107053, ans);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(88371, ans);
+    }
+}
