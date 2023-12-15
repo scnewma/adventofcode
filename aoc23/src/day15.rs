f56202f2aaa24191efb60c04ea810160fb7bd5ba
Shaun Newman <newman350z@gmail.com>
2023-12-14 22:54:01 -0700

    2023: day 15 solution

--- aoc23/src/day15.rs
+++ aoc23/src/day15.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +7,74 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    Ok(input.trim().split(',').map(hash).sum())
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    let mut hashmap = vec![VecDeque::<(&str, usize)>::default(); 256];
+    for word in input.trim().split(',') {
+        match word.split_once('=') {
+            Some((label, rest)) => {
+                let v: usize = rest.parse().unwrap();
+                let bx = &mut hashmap[hash(label)];
+
+                bx.iter_mut()
+                    .find(|(k, _)| *k == label)
+                    .and_then(|(_, val)| {
+                        *val = v;
+                        Some(())
+                    })
+                    .or_else(|| {
+                        bx.push_back((label, v));
+                        None
+                    });
+            }
+            None => {
+                assert!(word.ends_with('-'));
+                let label = &word[..word.len() - 1];
+                let bx = &mut hashmap[hash(label)];
+
+                bx.iter()
+                    .position(|(k, _)| k == &label)
+                    .and_then(|i| bx.remove(i));
+            }
+        }
+    }
+
+    let focusing_power = hashmap
+        .iter()
+        .enumerate()
+        .map(|(i, bx)| {
+            bx.iter()
+                .enumerate()
+                .map(|(j, (_, v))| (i + 1) * (j + 1) * v)
+                .sum::<usize>()
+        })
+        .sum();
+    Ok(focusing_power)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn hash(s: &str) -> usize {
+    s.chars()
+        .fold(0, |acc, ch| ((acc + ch as usize) * 17) % 256)
+}
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day15.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day15.input.txt");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+    const INPUT: &'static str = include_str!("../inputs/day15.input.txt");
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(513214, ans);
+    }
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
-
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(258826, ans);
+    }
+}
