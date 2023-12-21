ff85f1da9f7b6b445e59322e38d1a6475aee5a08
Shaun Newman <newman350z@gmail.com>
2023-12-20 22:42:50 -0700

    2023: day 21 part 1 solution

--- aoc23/src/day21.rs
+++ aoc23/src/day21.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+
+use itertools::{iproduct, Itertools};
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,8 +9,42 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+const DELTAS: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    let grid = input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+
+    let (height, width) = (grid.len(), grid[0].len());
+
+    let mut positions = Vec::new();
+    for (row, col) in iproduct!(0..height, 0..width) {
+        if grid[row][col] == 'S' {
+            positions.push((row as i32, col as i32));
+        }
+    }
+
+    for _ in 0..64 {
+        let mut next_positions = HashSet::new();
+        while let Some((row, col)) = positions.pop() {
+            for (dr, dc) in DELTAS {
+                let (r, c) = (row + dr, col + dc);
+                if r < 0
+                    || r >= height as i32
+                    || c < 0
+                    || c >= width as i32
+                    || grid[r as usize][c as usize] == '#'
+                {
+                    continue;
+                }
+                next_positions.insert((r, c));
+            }
+        }
+        positions = next_positions.into_iter().collect_vec();
+    }
+    Ok(positions.len())
 }
 
 pub fn part02(_input: &str) -> anyhow::Result<i64> {
