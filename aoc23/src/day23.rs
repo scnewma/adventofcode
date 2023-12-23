a3d924f7bc936725e15e274e1bb7cedbc57c9ac3
Shaun Newman <newman350z@gmail.com>
2023-12-23 00:13:46 -0700

    2023: day 23 solution
    part 2 takes 6 minutes to brute-force

--- aoc23/src/day23.rs
+++ aoc23/src/day23.rs
@@ -1,3 +1,7 @@
+use std::collections::BinaryHeap;
+
+use fxhash::FxHashSet;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +9,125 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let start = (0, grid[0].iter().position(|&c| c == '.').unwrap());
+    let end = (
+        grid.len() - 1,
+        grid[grid.len() - 1].iter().position(|&c| c == '.').unwrap(),
+    );
+    Ok(longest_path(&grid, start, end))
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    println!("part02");
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|ch| match ch {
+                    '^' | '>' | '<' | 'v' => '.',
+                    ch => ch,
+                })
+                .collect()
+        })
+        .collect();
+    let start = (0, grid[0].iter().position(|&c| c == '.').unwrap());
+    let end = (
+        grid.len() - 1,
+        grid[grid.len() - 1].iter().position(|&c| c == '.').unwrap(),
+    );
+    Ok(longest_path_dfs(
+        &grid,
+        &mut FxHashSet::default(),
+        start,
+        end,
+    ))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn longest_path(grid: &Vec<Vec<char>>, start: (usize, usize), end: (usize, usize)) -> usize {
+    let mut queue = BinaryHeap::new();
+    queue.push((0, start, vec![]));
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day23.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day23.input.txt");
+    let mut distances = vec![vec![0; grid[0].len()]; grid.len()];
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
+    while let Some((dist, (row, col), path)) = queue.pop() {
+        distances[row][col] = distances[row][col].max(dist);
+        let deltas = match grid[row][col] {
+            '>' => vec![(0, 1)],
+            '<' => vec![(0, -1)],
+            'v' => vec![(1, 0)],
+            '^' => vec![(-1, 0)],
+            _ => vec![(-1, 0), (1, 0), (0, -1), (0, 1)],
+        };
 
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
+        for (dr, dc) in deltas {
+            let (nr, nc) = (row as i32 + dr, col as i32 + dc);
 
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
+            if nr < 0 || nr >= grid.len() as i32 || nc < 0 || nc >= grid[0].len() as i32 {
+                continue;
+            }
 
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+            let (nr, nc) = (nr as usize, nc as usize);
+            if grid[nr][nc] == '#' || path.contains(&(nr, nc)) {
+                continue;
+            }
+
+            let mut path = path.clone();
+            path.push((row, col));
+            queue.push((dist + 1, (nr, nc), path));
+        }
+    }
+    distances[end.0][end.1]
+}
+
+fn longest_path_dfs(
+    grid: &Vec<Vec<char>>,
+    visited: &mut FxHashSet<(usize, usize)>,
+    start: (usize, usize),
+    end: (usize, usize),
+) -> usize {
+    if start == end {
+        return visited.len();
+    }
+
+    let (row, col) = start;
+    let mut longest_path = 0;
+    for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let (nr, nc) = (row as i32 + dr, col as i32 + dc);
+        if nr < 0 || nr >= grid.len() as i32 || nc < 0 || nc >= grid[0].len() as i32 {
+            continue;
+        }
+
+        let (nr, nc) = (nr as usize, nc as usize);
+        if grid[nr][nc] == '#' {
+            continue;
+        }
+
+        if !visited.insert((nr, nc)) {
+            continue;
+        }
+        longest_path = longest_path.max(longest_path_dfs(&grid, visited, (nr, nc), end));
+        visited.remove(&(nr, nc));
+    }
+    longest_path
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = include_str!("../inputs/day23.input.txt");
+
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(2314, ans);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(6874, ans);
+    }
+}
