a0eeb9724d6ac4903525c5d89d71057bf8b4bf3f
Shaun Newman <newman350z@gmail.com>
2023-12-24 23:28:03 -0700

    2023: day 25 solution
    cheated by drawing a "dot" graph and then plugging in the obvious edges
    that connected the two halves. :shrug:
    
    i started going down this path, but it didn't give the right answer
    * https://en.wikipedia.org/wiki/Biconnected_component
    
    i think this is the correct "real" solution:
    * https://en.wikipedia.org/wiki/Stoer%E2%80%93Wagner_algorithm

--- aoc23/src/day25.rs
+++ aoc23/src/day25.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -5,42 +7,67 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    let mut edges = HashSet::new();
+    let mut nodes = HashSet::new();
+    for line in input.lines() {
+        let (lhs, rhs) = line.split_once(": ").unwrap();
+        nodes.insert(lhs.to_string());
+        rhs.split_whitespace().for_each(|s| {
+            nodes.insert(s.to_string());
+            let mut v = vec![lhs.to_string(), s.to_string()];
+            v.sort();
+            edges.insert((v[0].clone(), v[1].clone()));
+        });
+    }
+
+    // print the dot graph
+    // println!("graph {{");
+    // for (n1, n2) in edges.iter() {
+    //     println!("  {} -- {};", n1, n2);
+    // }
+    // println!("}}");
+
+    // sample
+    // for e in [("jqt", "nvd"), ("hfx", "pzl"), ("bvb", "cmg")] {
+    for e in [("gsk", "ncg"), ("gmr", "ntx"), ("mrd", "rjs")] {
+        edges.remove(&(e.0.to_string(), e.1.to_string()));
+    }
+
+    let mut queue = VecDeque::new();
+    queue.push_back(nodes.iter().next().unwrap().as_str());
+
+    let mut visited = HashSet::new();
+
+    while let Some(node) = queue.pop_front() {
+        if !visited.insert(node) {
+            continue;
+        }
+
+        for edge in edges.iter() {
+            if edge.0 == node || edge.1 == node {
+                queue.push_back(if edge.0 == node { &edge.1 } else { &edge.0 });
+            }
+        }
+    }
+
+    Ok(visited.len() * (nodes.len() - visited.len()))
 }
 
+// needs to stay here because of benchmarks
 pub fn part02(_input: &str) -> anyhow::Result<i64> {
     Ok(0)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     const SAMPLE: &'static str = include_str!("../inputs/day25.sample.txt");
-//     const INPUT: &'static str = include_str!("../inputs/day25.input.txt");
+    const INPUT: &'static str = include_str!("../inputs/day25.input.txt");
 
-//     #[test]
-//     fn test_part_one_sample() {
-//         let ans = part01(SAMPLE).unwrap();
-//         assert_eq!(24000, ans);
-//     }
-
-//     #[test]
-//     fn test_part_one() {
-//         let ans = part01(INPUT).unwrap();
-//         assert_eq!(69501, ans);
-//     }
-
-//     #[test]
-//     fn test_part_two_sample() {
-//         let ans = part02(SAMPLE).unwrap();
-//         assert_eq!(45000, ans);
-//     }
-
-//     #[test]
-//     fn test_part_two() {
-//         let ans = part02(INPUT).unwrap();
-//         assert_eq!(202346, ans);
-//     }
-// }
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(580800, ans);
+    }
+}
