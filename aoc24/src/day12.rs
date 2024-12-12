d57cdd816191242e1ea0d9363357b531042b0fa8
Shaun Newman <newman350z@gmail.com>
2024-12-12 10:32:01 -0700

    lots of simplification

--- aoc24/src/day12.rs
+++ aoc24/src/day12.rs
@@ -11,14 +11,7 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
 }
 
 pub fn part01(input: &str) -> anyhow::Result<usize> {
-    let height = input.lines().count();
-    let width = input.lines().next().unwrap().len();
-    let mut grid = FxHashMap::default();
-    for (r, line) in input.lines().enumerate() {
-        for (c, ch) in line.char_indices() {
-            grid.insert((r as isize, c as isize), ch);
-        }
-    }
+    let (grid, height, width) = parse_input(input);
 
     let mut sum = 0;
     let mut visited = FxHashSet::default();
@@ -41,8 +34,8 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
                 continue;
             }
             perimeter += [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
-                .iter()
-                .filter(|&&(r, c)| grid.get(&(r, c)).is_none_or(|ch| *ch != region_ch))
+                .into_iter()
+                .filter(|&(r, c)| grid.get(&(r, c)).is_none_or(|ch| *ch != region_ch))
                 .count();
 
             area += 1;
@@ -59,6 +52,71 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
 }
 
 pub fn part02(input: &str) -> anyhow::Result<usize> {
+    let (grid, height, width) = parse_input(input);
+
+    let mut sum = 0;
+    let mut visited = FxHashSet::default();
+    for (r, c) in iproduct!(0..height as isize, 0..width as isize) {
+        if visited.contains(&(r, c)) {
+            continue;
+        }
+
+        let region_ch = grid[&(r, c)];
+
+        // helper function
+        let is_region_ch =
+            |r: isize, c: isize| grid.get(&(r, c)).is_some_and(|ch| *ch == region_ch);
+
+        let mut q = VecDeque::new();
+        q.push_back((r, c));
+
+        let mut area = 0;
+
+        let mut vertices = FxHashSet::default();
+        #[derive(Hash, PartialEq, Eq)]
+        enum Corner {
+            TopLeft,
+            TopRight,
+            BotLeft,
+            BotRight,
+        }
+
+        while let Some((r, c)) = q.pop_front() {
+            if grid.get(&(r, c)).is_none_or(|ch| *ch != region_ch) {
+                continue;
+            }
+            if !visited.insert((r, c)) {
+                continue;
+            }
+            let vertex_checks = [
+                (Corner::TopLeft, (-1, -1)),
+                (Corner::TopRight, (-1, 1)),
+                (Corner::BotLeft, (1, 1)),
+                (Corner::BotRight, (1, -1)),
+            ];
+            for (corner, (dr, dc)) in vertex_checks {
+                let (nr, nc) = (r + dr, c + dc);
+                let is_inner_corner =
+                    is_region_ch(nr, c) && is_region_ch(r, nc) && !is_region_ch(nr, nc);
+                let is_outer_corner = !is_region_ch(nr, c) && !is_region_ch(r, nc);
+                if is_inner_corner || is_outer_corner {
+                    vertices.insert((corner, r, c));
+                }
+            }
+            area += 1;
+            q.push_back((r + 1, c));
+            q.push_back((r - 1, c));
+            q.push_back((r, c + 1));
+            q.push_back((r, c - 1));
+        }
+
+        sum += area * vertices.len();
+    }
+
+    Ok(sum)
+}
+
+fn parse_input(input: &str) -> (FxHashMap<(isize, isize), char>, usize, usize) {
     let height = input.lines().count();
     let width = input.lines().next().unwrap().len();
     let mut grid = FxHashMap::default();
@@ -67,107 +125,7 @@ pub fn part02(input: &str) -> anyhow::Result<usize> {
             grid.insert((r as isize, c as isize), ch);
         }
     }
-
-    let mut sum = 0;
-    let mut visited = FxHashSet::default();
-    for (r, c) in iproduct!(0..height, 0..width) {
-        if visited.contains(&(r as isize, c as isize)) {
-            continue;
-        }
-        let r = r as isize;
-        let c = c as isize;
-
-        let region_ch = grid[&(r, c)];
-
-        let mut q = VecDeque::new();
-        q.push_back((r, c));
-
-        let mut area = 0;
-        let mut points = FxHashSet::default();
-        let mut min_r = 0;
-        let mut min_c = 0;
-        let mut max_r = height as isize;
-        let mut max_c = width as isize;
-        while let Some((r, c)) = q.pop_front() {
-            if grid.get(&(r, c)).is_none_or(|ch| *ch != region_ch) {
-                continue;
-            }
-            if !visited.insert((r, c)) {
-                continue;
-            }
-            points.insert((r, c));
-            min_r = min_r.min(r);
-            min_c = min_c.min(c);
-            max_r = max_r.max(r);
-            max_c = max_c.max(c);
-
-            area += 1;
-            q.push_back((r + 1, c));
-            q.push_back((r - 1, c));
-            q.push_back((r, c + 1));
-            q.push_back((r, c - 1));
-        }
-
-        // sets of vertices (edges = count of vertices)
-        let mut tlv = FxHashSet::default();
-        let mut trv = FxHashSet::default();
-        let mut blv = FxHashSet::default();
-        let mut brv = FxHashSet::default();
-        for (r, c) in iproduct!(min_r - 1..=max_r + 1, min_c - 1..=max_c + 1) {
-            if points.contains(&(r, c)) {
-                let top_left_corner = points.contains(&(r + 1, c + 1))
-                    && !points.contains(&(r + 1, c))
-                    && !points.contains(&(r, c + 1));
-                if top_left_corner {
-                    tlv.insert((r + 1, c + 1));
-                }
-                let top_right_corner = points.contains(&(r + 1, c - 1))
-                    && !points.contains(&(r + 1, c))
-                    && !points.contains(&(r, c - 1));
-                if top_right_corner {
-                    trv.insert((r + 1, c - 1));
-                }
-                let bot_left_corner = points.contains(&(r - 1, c + 1))
-                    && !points.contains(&(r - 1, c))
-                    && !points.contains(&(r, c + 1));
-                if bot_left_corner {
-                    blv.insert((r - 1, c + 1));
-                }
-                let bot_right_corner = points.contains(&(r - 1, c - 1))
-                    && !points.contains(&(r - 1, c))
-                    && !points.contains(&(r, c - 1));
-                if bot_right_corner {
-                    brv.insert((r - 1, c - 1));
-                }
-                continue;
-            }
-            let top_left_corner = points.contains(&(r + 1, c + 1))
-                && points.contains(&(r + 1, c)) == points.contains(&(r, c + 1));
-            if top_left_corner {
-                tlv.insert((r + 1, c + 1));
-            }
-            let top_right_corner = points.contains(&(r + 1, c - 1))
-                && points.contains(&(r + 1, c)) == points.contains(&(r, c - 1));
-            if top_right_corner {
-                trv.insert((r + 1, c - 1));
-            }
-            let bot_left_corner = points.contains(&(r - 1, c + 1))
-                && points.contains(&(r - 1, c)) == points.contains(&(r, c + 1));
-            if bot_left_corner {
-                blv.insert((r - 1, c + 1));
-            }
-            let bot_right_corner = points.contains(&(r - 1, c - 1))
-                && points.contains(&(r - 1, c)) == points.contains(&(r, c - 1));
-            if bot_right_corner {
-                brv.insert((r - 1, c - 1));
-            }
-        }
-
-        let num_sides = tlv.len() + trv.len() + blv.len() + brv.len();
-        sum += area * num_sides;
-    }
-
-    Ok(sum)
+    (grid, height, width)
 }
 
 #[cfg(test)]
