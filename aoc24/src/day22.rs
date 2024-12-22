794989ef6d0f414f1e044a38afcea813d6699f82
Shaun Newman <newman350z@gmail.com>
2024-12-22 09:51:03 -0700

    24,22,2: sigh
    I thought about this problem way too hard (and apparently suck at
    reading comprehension). At  first I was trying to take the best sell for
    any given monkey (but they always immediately sell when they find the
    sequence). Then when that didn't work, I assumed it was because you
    could not have multiple sells for multiple different monkeys at the same
    change number (but the problem says "instantly" which I guess means you
    can sell multiple times with the same number). This ended with me coding
    a dfs solution that chose a monkey to sell to at any given change...
    
    So, I made the problem WAY harder...

--- aoc24/src/day22.rs
+++ aoc24/src/day22.rs
@@ -1,3 +1,8 @@
+use std::collections::VecDeque;
+
+use fxhash::FxHashMap;
+use itertools::Itertools;
+
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
         part01: part01(input)?.to_string(),
@@ -28,7 +33,48 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
 }
 
 pub fn part02(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let secret_numbers = input.lines().map(|line| line.parse::<usize>().unwrap());
+
+    let mut change_seq_prices = FxHashMap::<(isize, isize, isize, isize), usize>::default();
+
+    for (_, mut secret_number) in secret_numbers.enumerate() {
+        let mut first_sell = FxHashMap::<(isize, isize, isize, isize), usize>::default();
+        let mut price_changes = VecDeque::new();
+        let mut prev_price = secret_number % 10;
+        for _ in 0..2000 {
+            let a = secret_number * 64;
+            secret_number ^= a;
+            secret_number %= 16777216;
+
+            let b = secret_number / 32;
+            secret_number ^= b;
+            secret_number %= 16777216;
+
+            let c = secret_number * 2048;
+            secret_number ^= c;
+            secret_number %= 16777216;
+
+            let price = secret_number % 10;
+            price_changes.push_back(price as isize - prev_price as isize);
+            if price_changes.len() > 4 {
+                price_changes.pop_front();
+            }
+            if let Some((a, b, c, d)) = price_changes.iter().collect_tuple() {
+                if !first_sell.contains_key(&(*a, *b, *c, *d)) {
+                    first_sell.insert((*a, *b, *c, *d), price);
+                }
+            }
+            prev_price = price;
+        }
+        for (k, v) in first_sell {
+            change_seq_prices
+                .entry(k)
+                .and_modify(|e| *e += v)
+                .or_insert(v);
+        }
+    }
+
+    return Ok(*change_seq_prices.values().max().unwrap());
 }
 
 #[cfg(test)]
@@ -40,12 +86,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let ans = part01(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(13584398738, ans);
     }
 
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(1612, ans);
     }
 }
