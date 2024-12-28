bdd9204be0cec2d0b9ed7cea0437e0b465d5b31f
Shaun Newman <newman350z@gmail.com>
2024-12-28 10:33:09 -0700

    19: structure

---
+++ aoc19/src/day01.rs
@@ -0,0 +1,33 @@
+pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
+    Ok(crate::SolveInfo {
+        part01: part01(input)?.to_string(),
+        part02: part02(input)?.to_string(),
+    })
+}
+
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    Ok(0)
+}
+
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("../inputs/day01.input.txt");
+
+    #[test]
+    fn test_part_one() {
+        let ans = part01(INPUT).unwrap();
+        assert_eq!(0, ans);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let ans = part02(INPUT).unwrap();
+        assert_eq!(0, ans);
+    }
+}
