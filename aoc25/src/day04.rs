928752fa5e44b8e5062438025278f5f672485223
Shaun Newman <newman350z@gmail.com>
2025-12-17 17:12:22 -0600

    2025 day 4 p 2

--- aoc25/src/day04.rs
+++ aoc25/src/day04.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
-use itertools::iproduct;
+use crate::DELTAS8;
 
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
@@ -10,42 +10,17 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
 }
 
 pub fn part01(input: &str) -> anyhow::Result<usize> {
-    let mut grid = HashMap::new();
-    let h = input.lines().count() as i32;
-    let w = input.lines().next().unwrap().chars().count() as i32;
-    for (r, line) in input.lines().enumerate() {
-        for (c, ch) in line.chars().enumerate() {
-            grid.insert((r as i32, c as i32), ch);
-        }
-    }
-
-    const DELTAS: &[(i32, i32)] = &[
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
+    let rolls = parse_input(input);
 
     let mut accessible_rolls = 0;
-    for (r, c) in iproduct!(0..h as i32, 0..w as i32) {
-        if grid[&(r, c)] != '@' {
-            continue;
-        }
-
-        let mut count = 0;
-        for (dr, dc) in DELTAS {
-            let (nr, nc) = (r + dr, c + dc);
-            if nr < 0 || nr > h - 1 || nc < 0 || nc > w - 1 {
-                continue;
-            }
-            if grid[&(nr, nc)] == '@' {
-                count += 1;
-            }
-        }
+    for (r, c) in rolls.iter() {
+        let count: usize = DELTAS8
+            .into_iter()
+            .map(|(dr, dc)| {
+                let npos = (r + dr, c + dc);
+                if rolls.contains(&npos) { 1 } else { 0 }
+            })
+            .sum();
         if count < 4 {
             accessible_rolls += 1;
         }
@@ -54,7 +29,45 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
 }
 
 pub fn part02(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let mut rolls = parse_input(input);
+
+    let mut rolls_removed = 0;
+    loop {
+        let mut rolls_removed_turn = 0;
+
+        for (r, c) in rolls.clone().iter() {
+            let count: usize = DELTAS8
+                .into_iter()
+                .map(|(dr, dc)| {
+                    let npos = (r + dr, c + dc);
+                    if rolls.contains(&npos) { 1 } else { 0 }
+                })
+                .sum();
+            if count < 4 {
+                rolls.remove(&(*r, *c));
+                rolls_removed_turn += 1;
+            }
+        }
+
+        if rolls_removed_turn == 0 {
+            break;
+        }
+        rolls_removed += rolls_removed_turn;
+    }
+
+    Ok(rolls_removed)
+}
+
+fn parse_input(input: &str) -> HashSet<(isize, isize)> {
+    let mut grid = HashSet::new();
+    for (r, line) in input.lines().enumerate() {
+        for (c, ch) in line.chars().enumerate() {
+            if ch == '@' {
+                grid.insert((r as isize, c as isize));
+            }
+        }
+    }
+    grid
 }
 
 #[cfg(test)]
@@ -66,12 +79,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let ans = part01(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(1367, ans);
     }
 
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(9144, ans);
     }
 }
