5901d8fb9acbcc7e88f893c976b2ad72e04bdc9d
Shaun Newman <newman350z@gmail.com>
2025-12-19 17:24:21 -0600

    2025 day 8 p 2

--- aoc25/src/day08.rs
+++ aoc25/src/day08.rs
@@ -1,10 +1,7 @@
 use itertools::Itertools;
 use ordered_float::NotNan;
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashMap},
-};
-use union_find::{QuickUnionUf, UnionBySize, UnionFind};
+use std::{cmp::Reverse, collections::HashMap};
+use union_find::{QuickFindUf, QuickUnionUf, UnionBySize, UnionFind};
 
 pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     Ok(crate::SolveInfo {
@@ -21,16 +18,10 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
         1000
     };
 
-    // parse input
-    let mut boxes = Vec::new();
-    for line in input.lines() {
-        let nums: Vec<usize> = line.splitn(3, ',').map(|s| s.parse().unwrap()).collect();
-        boxes.push((nums[0], nums[1], nums[2])); // x, y, z
-    }
+    let boxes = parse_input(input);
 
     // sort all pairs by distance
-    let mut pairs = HashMap::new(); // stores pairs by distance for later lookup
-    let mut heap = BinaryHeap::new();
+    let mut pairs = Vec::with_capacity(499500); // 1000 choose 2
     for combo in boxes.clone().into_iter().combinations(2) {
         let (a, b) = (combo[0], combo[1]);
         let dx = a.0 as f64 - b.0 as f64;
@@ -39,41 +30,76 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
 
         let d = (dx.powi(2) + dy.powi(2) + dz.powi(2)).sqrt();
         let d = NotNan::new(d).unwrap();
-        heap.push(Reverse(d));
-        assert!(pairs.insert(d, (a, b)).is_none());
+        pairs.push((a, b, d));
     }
+    pairs.sort_by_key(|(_, _, d)| *d);
 
     // use union-find to connect the shortest pairs
     let mut uf = QuickUnionUf::<UnionBySize>::new(boxes.len());
-    for _ in 0..n_shortest_connections {
-        if let Some(Reverse(d)) = heap.pop() {
-            let (a, b) = pairs.get(&d).unwrap();
-            let ai = boxes.iter().position(|x| x == a).unwrap();
-            let bi = boxes.iter().position(|x| x == b).unwrap();
-            uf.union(ai, bi);
-        }
+    for (a, b, _) in &pairs[0..n_shortest_connections] {
+        let ai = boxes.iter().position(|x| x == a).unwrap();
+        let bi = boxes.iter().position(|x| x == b).unwrap();
+        uf.union(ai, bi);
     }
 
-    // built circuits based on where union-find algo put them
+    // build circuits based on where union-find algo put them
     let mut circuits = HashMap::new();
     (0..boxes.len()).for_each(|i| {
         _ = circuits
             .entry(uf.find(i))
-            .and_modify(|pairs: &mut Vec<_>| pairs.push(&boxes[i]))
-            .or_insert(vec![&boxes[i]])
+            .and_modify(|len| *len+=1)
+            .or_insert(1)
     });
 
     // product of top3 circuit sizes
     Ok(circuits
         .values()
-        .map(|v| v.len())
         .sorted_by_key(|l| Reverse(*l))
         .take(3)
         .product::<usize>())
 }
 
 pub fn part02(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let boxes = parse_input(input);
+
+    // sort all pairs by distance
+    let mut pairs = Vec::with_capacity(499500); // 1000 choose 2
+    for combo in boxes.clone().into_iter().combinations(2) {
+        let (a, b) = (combo[0], combo[1]);
+        let dx = a.0 as f64 - b.0 as f64;
+        let dy = a.1 as f64 - b.1 as f64;
+        let dz = a.2 as f64 - b.2 as f64;
+
+        let d = (dx.powi(2) + dy.powi(2) + dz.powi(2)).sqrt();
+        let d = NotNan::new(d).unwrap();
+        pairs.push((a, b, d));
+    }
+    pairs.sort_by_key(|(_, _, d)| *d);
+
+    // union pairs until all boxes connected
+    let mut uf = QuickFindUf::<UnionBySize>::new(boxes.len());
+    for (a, b, _) in pairs {
+        let ai = boxes.iter().position(|x| *x == a).unwrap();
+        let bi = boxes.iter().position(|x| *x == b).unwrap();
+        if !uf.union(ai, bi) {
+            // boxes were in same set, no need to check for change
+            continue;
+        }
+
+        if (0..boxes.len()).map(|i| uf.find(i)).all_equal() {
+            return Ok(a.0 * b.0);
+        }
+    }
+    unreachable!()
+}
+
+fn parse_input(input: &str) -> Vec<(usize, usize, usize)> {
+    let mut boxes = Vec::new();
+    for line in input.lines() {
+        let nums: Vec<usize> = line.splitn(3, ',').map(|s| s.parse().unwrap()).collect();
+        boxes.push((nums[0], nums[1], nums[2])); // x, y, z
+    }
+    boxes
 }
 
 #[cfg(test)]
@@ -91,6 +117,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(107256172, ans);
     }
 }
