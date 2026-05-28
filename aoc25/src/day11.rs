993ef47a22f88872f1282c78639d0a916b8fabe1
Shaun Newman <newman350z@gmail.com>
2026-05-27 23:25:46 -0500

    2025,11,2: topological walk with masked dp

--- aoc25/src/day11.rs
+++ aoc25/src/day11.rs
@@ -15,28 +15,89 @@ pub fn part01(input: &str) -> anyhow::Result<usize> {
         graph.insert(src, dests);
     }
 
-    let mut completed_paths = HashSet::new();
+    let mut completed_paths = 0;
     let mut q = VecDeque::new();
-    for v in graph.get("you").unwrap() {
-        q.push_back(vec![*v]);
+    for v in graph.get("you").unwrap_or(&vec![]) {
+        q.push_back(*v);
     }
-    while let Some(path) = q.pop_front() {
-        let v = *path.last().unwrap();
+    while let Some(v) = q.pop_front() {
         if v == "out" {
-            completed_paths.insert(path);
+            completed_paths += 1;
             continue;
         }
         for u in graph.get(v).unwrap() {
-            let mut next = path.clone();
-            next.push(u);
-            q.push_back(next);
+            q.push_back(u);
         }
     }
-    Ok(completed_paths.len())
+    Ok(completed_paths)
 }
 
 pub fn part02(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut nodes = HashSet::new();
+    let mut in_degree = HashMap::new();
+    for line in input.lines() {
+        let (src, dests) = line.split_once(": ").unwrap();
+        nodes.insert(src);
+        let dests: Vec<_> = dests.split_whitespace().collect();
+        for dest in dests {
+            graph.entry(src).or_default().push(dest);
+            nodes.insert(dest);
+            in_degree.entry(dest).and_modify(|n| *n += 1).or_insert(1);
+        }
+    }
+
+    let mut queue = VecDeque::new();
+    for v in nodes.iter() {
+        if *in_degree.get(v).unwrap_or(&0) == 0 {
+            queue.push_back(*v);
+        }
+    }
+
+    let mut ways: HashMap<(&str, u8), usize> = HashMap::new();
+    ways.insert(("svr", 0), 1);
+
+    const MASKS: [u8; 4] = [
+        0b00, // none seen
+        0b01, // dac seen
+        0b10, // fft seen
+        0b11, // dac and fft seen
+    ];
+
+    // walk the graph topologically
+    while let Some(v) = queue.pop_front() {
+        for mask in MASKS {
+            let count = *ways.get(&(v, mask)).unwrap_or(&0);
+            if count == 0 {
+                continue;
+            }
+
+            if let Some(children) = graph.get(v) {
+                for child in children {
+                    let mut next_mask = mask;
+                    if *child == "dac" {
+                        next_mask |= 0b01;
+                    }
+                    if *child == "fft" {
+                        next_mask |= 0b10;
+                    }
+                    *ways.entry((child, next_mask)).or_insert(0) += count;
+                }
+            }
+        }
+
+        // advance topological walk
+        if let Some(children) = graph.get(v) {
+            for child in children {
+                in_degree.entry(child).and_modify(|n| *n -= 1);
+                if in_degree[child] == 0 {
+                    queue.push_back(child);
+                }
+            }
+        }
+    }
+
+    Ok(*ways.get(&("out", 0b11)).expect("no solution"))
 }
 
 #[cfg(test)]
@@ -54,6 +115,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(372918445876116, ans);
     }
 }
