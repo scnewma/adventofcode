af4921f63d44cddc26ce6f39598645b559dbad1b
Shaun Newman <newman350z@gmail.com>
2026-05-29 16:20:07 -0500

    2020,05: solve, no allocs

--- aoc20/src/day05.rs
+++ aoc20/src/day05.rs
@@ -5,12 +5,36 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    Ok(iter_seats(input).fold(0, |acc, seat_id| acc.max(seat_id)))
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    let (min, max, sum) = iter_seats(input).fold((usize::MAX, 0, 0), |(min, max, sum), seat_id| {
+        (min.min(seat_id), max.max(seat_id), sum + seat_id)
+    });
+    let expected_sum = (min + max) * (max - min + 1) / 2;
+    Ok(expected_sum - sum)
+}
+
+fn iter_seats(input: &str) -> impl Iterator<Item = usize> {
+    input.lines().map(|boarding_pass| {
+        let row = locate(&boarding_pass[..7], 127);
+        let col = locate(&boarding_pass[7..], 7);
+        row * 8 + col
+    })
+}
+
+fn locate(path: &str, max: usize) -> usize {
+    let (mut lo, mut hi) = (0, max);
+    for c in path.chars() {
+        match c {
+            'F' | 'L' => hi = lo + (hi - lo) / 2,
+            'B' | 'R' => lo = hi - (hi - lo) / 2,
+            _ => unreachable!("character {c} unknown"),
+        }
+    }
+    lo
 }
 
 #[cfg(test)]
@@ -22,12 +46,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let ans = part01(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(816, ans);
     }
 
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(539, ans);
     }
 }
