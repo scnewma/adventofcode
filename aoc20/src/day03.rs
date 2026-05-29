2cb2c79db6de4cad116e7970023635d8473cca0a
Shaun Newman <newman350z@gmail.com>
2026-05-29 11:14:45 -0500

    2020,03: solve, no allocations

--- aoc20/src/day03.rs
+++ aoc20/src/day03.rs
@@ -5,12 +5,28 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    Ok(count_arboreal_stops(input, 3, 1))
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    Ok([(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .into_iter()
+        .map(|(dx, dy)| count_arboreal_stops(input, dx, dy))
+        .product())
+}
+
+fn count_arboreal_stops(input: &str, dx: usize, dy: usize) -> usize {
+    let width = input.lines().next().unwrap().len();
+    let mut trees = 0;
+    let mut col = 0;
+    for line in input.lines().step_by(dy).skip(1) {
+        col += dx;
+        if line.as_bytes()[col % width] == b'#' {
+            trees += 1
+        }
+    }
+    trees
 }
 
 #[cfg(test)]
@@ -22,12 +38,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let ans = part01(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(216, ans);
     }
 
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(6708199680, ans);
     }
 }
