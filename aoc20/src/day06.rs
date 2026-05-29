06f1bd4a95797e0a6ba9005ecebad9d94c848ce2
Shaun Newman <newman350z@gmail.com>
2026-05-29 16:42:40 -0500

    2020,06: solve, no allocs

--- aoc20/src/day06.rs
+++ aoc20/src/day06.rs
@@ -5,12 +5,33 @@ pub fn run(input: &str) -> anyhow::Result<crate::SolveInfo> {
     })
 }
 
-pub fn part01(_input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+pub fn part01(input: &str) -> anyhow::Result<usize> {
+    Ok(solve::<true>(input))
 }
 
-pub fn part02(_input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+pub fn part02(input: &str) -> anyhow::Result<usize> {
+    Ok(solve::<false>(input))
+}
+
+pub fn solve<const ANYONE: bool>(input: &str) -> usize {
+    let mut sum = 0;
+    for group in input.split("\n\n") {
+        let mut group_mask = if ANYONE { 0u32 } else { u32::MAX };
+        for line in group.lines() {
+            let mut person_mask = 0u32;
+            for c in line.chars() {
+                person_mask |= 1 << (c as u32 - 'a' as u32);
+            }
+
+            if ANYONE {
+                group_mask |= person_mask;
+            } else {
+                group_mask &= person_mask;
+            }
+        }
+        sum += group_mask.count_ones();
+    }
+    sum as usize
 }
 
 #[cfg(test)]
@@ -22,12 +43,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let ans = part01(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(6612, ans);
     }
 
     #[test]
     fn test_part_two() {
         let ans = part02(INPUT).unwrap();
-        assert_eq!(0, ans);
+        assert_eq!(3268, ans);
     }
 }
